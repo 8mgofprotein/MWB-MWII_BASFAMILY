355b00429da959fe040f26d4f3f0ca0264f712d7
ZeR0-N <tenppppp@hotmail.com>
2023-11-13 23:05:38 +0700

    teeny bit of fix

--- lua/weapons/mg_base/modules/attachmentss/mpapax/mags/attachment_vm_sm_mpapax_smag.lua
+++ lua/weapons/mg_base/modules/attachmentss/mpapax/mags/attachment_vm_sm_mpapax_smag.lua
@@ -20,7 +20,7 @@ local BaseClass = GetAttachmentBaseClass(ATTACHMENT.Base)
 function ATTACHMENT:Stats(weapon)
     BaseClass.Stats(self, weapon)
         weapon.Primary.ClipSize = 20
-        weapon.Animations.Reload = weapon.Animations.Reload_fast_smag
+        weapon.Animations.Reload = weapon.Animations.Reload_smag
 		weapon.Animations.Reload_Fast = weapon.Animations.Reload_fast_smag
         weapon.Animations.Reload_Empty = weapon.Animations.Reload_Empty_Smag
 		weapon.Animations.Reload_Empty_Fast = weapon.Animations.Reload_empty_fast_smag
