12714571f596912323cc8e57ce2e3c3de0ec4bb2
Neko-kyo <122740216+8mgofprotein@users.noreply.github.com>
2023-11-15 16:17:19 +0700

    chimera attachments

---
+++ lua/weapons/mg_base/modules/sounds/weapon_sm_mwii_mcbravo.lua
@@ -0,0 +1,717 @@
+
+sound.Add({
+    name =           "weap_ar_mcharlie_ads_down",
+    channel =        CHAN_WPNFOLEY +2,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar_mcharlie_ads_down.wav"}              
+})
+sound.Add({
+    name =           "weap_ar_mcharlie_ads_up",
+    channel =        CHAN_WPNFOLEY +3,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar_mcharlie_ads_up.wav"}              
+})
+
+
+-- Mike4 --
+sound.Add({
+    name =           "weap_mcbravo_fire_plr_shot_01",
+    channel =        CHAN_WEAPON,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_06.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_fire_plr_shot_07.wav"
+            }              
+})
+sound.Add({
+    name =           "weap_mcbravo_sup_plr_shot_01",
+    channel =        CHAN_WEAPON,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_06.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcbravo_sup_plr_shot_07.wav"
+            }              
+})
+sound.Add({
+    name =           "weap_mcharlie_fire_plr_shot_01",
+    channel =        CHAN_WEAPON,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_06.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_fire_plr_shot_07.wav"
+            }              
+})
+sound.Add({
+    name =           "weap_mcharlie_fire_plr_shot_01",
+    channel =        CHAN_WEAPON,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_06.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_mcharlie_sup_plr_shot_07.wav"
+            }              
+})
+sound.Add({
+    name =           "weap_ar_sup_fire_plr_atmo_int1_01",
+    channel =        CHAN_WEAPON,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar_sup_fire_plr_atmo_int1_01.wav",
+             "kyo/mwii/mcharliebraavorttle/weap_ar_sup_fire_plr_atmo_int1_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar_sup_fire_plr_atmo_int1_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar_sup_fire_plr_atmo_int1_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar_sup_fire_plr_atmo_int1_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar_sup_fire_plr_atmo_int1_06.wav"
+            }              
+})
+
+sound.Add({
+    name =           "weap_ar4_sup_fire_plr_atmo_ext1_01",
+    channel =        CHAN_ATMO,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar4_sup_fire_plr_atmo_ext1_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_ar4_sup_fire_plr_atmo_ext1_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar4_sup_fire_plr_atmo_ext1_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar4_sup_fire_plr_atmo_ext1_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar4_sup_fire_plr_atmo_ext1_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar4_sup_fire_plr_atmo_ext1_06.wav",
+            }              
+})
+sound.Add({
+    name =           "weap_ar6_sup_sub_fire_plr_atmo_ext1_01",
+    channel =        CHAN_ATMO,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar6_sup_sub_fire_plr_atmo_ext1_01.wav",
+             "kyo/mwii/mcharliebraavorttle/weap_ar6_sup_sub_fire_plr_atmo_ext1_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar6_sup_sub_fire_plr_atmo_ext1_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar6_sup_sub_fire_plr_atmo_ext1_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar6_sup_sub_fire_plr_atmo_ext1_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar6_sup_sub_fire_plr_atmo_ext1_06.wav",
+            }              
+})
+sound.Add({
+    name =           "weap_ar8_fire_plr_atmo_ext1_01",
+    channel =        CHAN_ATMO,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar8_fire_plr_atmo_ext1_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_ar8_fire_plr_atmo_ext1_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar8_fire_plr_atmo_ext1_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar8_fire_plr_atmo_ext1_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar8_fire_plr_atmo_ext1_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar8_fire_plr_atmo_ext1_06.wav",
+            }              
+})
+sound.Add({
+    name =           "weap_ar10_fire_plr_atmo_ext1_01",
+    channel =        CHAN_ATMO,
+    level = 140,
+    volume =         1,
+    sound = {"kyo/mwii/mcharliebraavorttle/weap_ar10_fire_plr_atmo_ext1_01.wav",
+	         "kyo/mwii/mcharliebraavorttle/weap_ar10_fire_plr_atmo_ext1_02.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar10_fire_plr_atmo_ext1_03.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar10_fire_plr_atmo_ext1_04.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar10_fire_plr_atmo_ext1_05.wav",
+			 "kyo/mwii/mcharliebraavorttle/weap_ar10_fire_plr_atmo_ext1_06.wav",
+            }              
+})
+-- Sound: 22
+sound.Add({
+	name = "wfoly_ar_mcharlie_drop",
+	channel = CHAN_WPNFOLEY + 7,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_drop.wav",
+		}
+})
+-- Sound: 23
+sound.Add({
+	name = "wfoly_ar_mcharlie_drop_quick",
+	channel = CHAN_WPNFOLEY + 8,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_drop_quick.wav",
+		}
+})
+-- Sound: 24
+sound.Add({
+	name = "wfoly_ar_mcharlie_inspect_01",
+	channel = CHAN_WPNFOLEY + 9,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_inspect_01.wav",
+		}
+})
+-- Sound: 25
+sound.Add({
+	name = "wfoly_ar_mcharlie_inspect_05",
+	channel = CHAN_WPNFOLEY + 10,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_inspect_05.wav",
+		}
+})
+-- Sound: 26
+sound.Add({
+	name = "wfoly_ar_mcharlie_raise",
+	channel = CHAN_WPNFOLEY + 1,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_raise.wav",
+		}
+})
+-- Sound: 27
+sound.Add({
+	name = "wfoly_ar_mcharlie_raise_quick",
+	channel = CHAN_WPNFOLEY + 2,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_raise_quick.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_bolt_release",
+	channel = CHAN_WPNFOLEY + 9,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_bolt_release.wav",
+		}
+})
+-- Sound: 35
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_end",
+	channel = CHAN_WPNFOLEY + 10,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_end.wav",
+		}
+})
+-- Sound: 36
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_fast_bolt_release_01",
+	channel = CHAN_WPNFOLEY + 1,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_fast_bolt_release_01.wav",
+		}
+})
+-- Sound: 37
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_fast_end",
+	channel = CHAN_WPNFOLEY + 2,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_fast_end.wav",
+		}
+})
+-- Sound: 38
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_fast_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_fast_magin_v2_01.wav",
+		}
+})
+-- Sound: 39
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_fast_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 4,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_fast_magin_v2_02.wav",
+		}
+})
+-- Sound: 40
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_fast_magout_01",
+	channel = CHAN_WPNFOLEY + 5,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_fast_magout_01.wav",
+		}
+})
+-- Sound: 41
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_fast_raise",
+	channel = CHAN_WPNFOLEY + 6,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_fast_raise.wav",
+		}
+})
+-- Sound: 42
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 7,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_magin_v2_01.wav",
+		}
+})
+-- Sound: 43
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 8,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_magin_v2_02.wav",
+		}
+})
+-- Sound: 44
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_magout_01",
+	channel = CHAN_WPNFOLEY + 9,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_magout_01.wav",
+		}
+})
+-- Sound: 45
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_mvmnt",
+	channel = CHAN_WPNFOLEY + 10,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_mvmnt.wav",
+		}
+})
+-- Sound: 46
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_empty_rotate",
+	channel = CHAN_WPNFOLEY + 1,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_empty_rotate.wav",
+		}
+})
+-- Sound: 47
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_end",
+	channel = CHAN_WPNFOLEY + 2,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_end.ogg",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_arm",
+	channel = CHAN_WPNFOLEY + 1,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_arm.wav",
+		}
+})
+-- Sound: 47
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_end",
+	channel = CHAN_WPNFOLEY + 2,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_end.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 1,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_magin_v2_01.wav",
+		}
+})
+-- Sound: 47
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 2,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_magin_v2_02.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_magout_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_magout_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_raise",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_raise.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_fast_rattle",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_fast_rattle.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_magin_v2_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_magin_v2_02.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_reload_magout_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_reload_magout_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_bolt_release",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_bolt_release.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_end",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_end.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_fast_bolt_release",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_fast_bolt_release.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_fast_end",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_fast_end.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_fast_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_fast_magin_v2_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_fast_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_fast_magin_v2_02.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_fast_magout_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_fast_magout_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_fast_raise",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_fast_raise.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_magin_v2_01.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_magin_v2_02.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_magout_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_magout_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_rotate",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_rotate.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_empty_settle",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_empty_settle.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_end",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_end.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_fast_arm",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_fast_arm.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_fast_end",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_fast_end.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_fast_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_fast_magin_v2_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_fast_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_fast_magin_v2_02.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_fast_magout_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_fast_magout_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_fast_rotate",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_fast_rotate.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_maghit",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_maghit.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_magin_v2_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_magin_v2_01.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_magin_v2_02",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_magin_v2_02.wav",
+		}
+})
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_magout_01",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_magout_01.wav",
+		}
+})
+
+sound.Add({
+	name = "wfoly_ar_mcharlie_xmag_reload_rotate",
+	channel = CHAN_WPNFOLEY + 3,
+	
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"kyo/mwii/mcharliebraavorttle/wfoly_ar_mcharlie_xmag_reload_rotate.wav",
+		}
+})
